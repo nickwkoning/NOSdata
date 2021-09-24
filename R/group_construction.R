20b08dc9c4c13d8fdd6aaeb7c2ee39076e0e61df
Nick Koning <nickwkoning@gmail.com>
2021-09-24 11:12:00 +0200

    Update group_construction.R

--- R/group_construction.R
+++ R/group_construction.R
@@ -57,3 +57,23 @@ for (n in 17:64) {
 }
 
 list.files("data-raw", full.names = TRUE) %>% map(source)
+
+
+leak_fun = leak_l1
+max_rank = 10
+max_order = 2^max_rank
+
+num_candidates = 100000
+for (n in 5:10) {
+  subgroup_list = construct_near_oracle_subgroup_path(n, num_candidates, max_rank, leak_fun)
+
+  name = paste0("l1_100k_subgroups_max", max_order, "_n", n)
+  assign(name, subgroup_list)
+  usethis::use_data_raw(name)
+
+  print(n)
+}
+
+list.files("data-raw", full.names = TRUE) %>% map(source)
+
+
