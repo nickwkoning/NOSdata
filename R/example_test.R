d45d7eeb1215d27d143a295990750f1c17503bd9
Nick Koning <nickwkoning@gmail.com>
2021-09-30 13:46:19 +0200

    commented some code away so package can be built

--- R/example_test.R
+++ R/example_test.R
@@ -1,17 +1,17 @@
-## Settings
-n = 10
-mu = .1
-rank = 6 # 2^(rank - 1) is the desired order of the subgroup
-alpha = .05
-
-# Generate some data
-x = mu + rnorm(n)
-
-# compute sign-flipped test statistics
-iGx = t(get(paste0("max_100k_subgroups_max1024_n", n))[[actual_rank]]) %*% x
-
-# find p-value
-p_val = mean(iGx[1] <= iGx)
-
-# rejection proportion
-mean(p_val < alpha)
+# ## Settings
+# n = 10
+# mu = .1
+# rank = 6 # 2^(rank - 1) is the desired order of the subgroup
+# alpha = .05
+#
+# # Generate some data
+# x = mu + rnorm(n)
+#
+# # compute sign-flipped test statistics
+# iGx = t(get(paste0("max_100k_subgroups_max1024_n", n))[[actual_rank]]) %*% x
+#
+# # find p-value
+# p_val = mean(iGx[1] <= iGx)
+#
+# # rejection proportion
+# mean(p_val < alpha)
--- R/group_construction_parallel.R
+++ R/group_construction_parallel.R
@@ -1,46 +1,47 @@
-library(NOS)
-library(usethis)
-library(devtools)
-library(tidyverse)
-library(doParallel)
-
-# parallelization parameters
-
-num_cores = parallel::detectCores() - 1
-
-my.cluster <- parallel::makeCluster(
-  num_cores,
-  type = "PSOCK"
-)
-
-print(my.cluster)
-
-doParallel::registerDoParallel(cl = my.cluster)
-
-# parameters
-
-
-leak_fun = leak_mean
-one_sided = TRUE
-max_rank = 10
-max_order = 2^max_rank
-num_candidates = 100000
-N_set = 1:256
-
-subgroup_list_list = foreach(
-  n = N_set,
-  .packages= c('NOS', 'tidyverse')
-) %dopar% {
-  subgroup_list = construct_near_oracle_subgroup_path(n, num_candidates, max_rank, leak_fun, one_sided)
-  subgroup_list
-}
-
-stopCluster(my.cluster)
-
-for (i in seq_along(N_set)) {
-  n = N_set[i]
-  name = paste0("l1_100k_subgroups_max", max_order, "_n", n)
-  assign(name, subgroup_list_list[[n - min(N_set) + 1]])
-  usethis::use_data_raw(name)
-}
-paste0("data-raw/l1_100k_subgroups_max", max_order, "_n", N_set, ".R") %>% map(source)
+# library(NOS)
+# library(usethis)
+# library(devtools)
+# library(tidyverse)
+# library(doParallel)
+#
+# # parallelization parameters
+#
+# num_cores = parallel::detectCores() - 1
+#
+# my.cluster <- parallel::makeCluster(
+#   num_cores,
+#   type = "PSOCK"
+# )
+#
+# print(my.cluster)
+#
+# doParallel::registerDoParallel(cl = my.cluster)
+#
+# # parameters
+#
+#
+# leak_fun = leak_linf
+# one_sided = TRUE
+# max_rank = 10
+# max_order = 2^max_rank
+# num_candidates = 100000
+# N_set = 1:256
+#
+# subgroup_list_list = foreach(
+#   n = N_set,
+#   .packages= c('NOS', 'tidyverse')
+# ) %dopar% {
+#   name = paste0("linf_100k_subgroups_max", max_order, "_n", n)
+#   usethis::use_data_raw(name)
+#   subgroup_list = construct_near_oracle_subgroup_path(n, num_candidates, max_rank, leak_fun, one_sided)
+#   subgroup_list
+# }
+#
+# stopCluster(my.cluster)
+#
+# for (i in seq_along(N_set)) {
+#   n = N_set[i]
+#   name = paste0("linf_100k_subgroups_max", max_order, "_n", n)
+#   assign(name, subgroup_list_list[[n - min(N_set) + 1]])
+# }
+# paste0("data-raw/linf_100k_subgroups_max", max_order, "_n", N_set, ".R") %>% map(source)
